453fead7c9baff7db75a1423784b51d2c2410178
Lilgonzo <56660056+Lilgonzo@users.noreply.github.com>
2021-12-07 17:30:18 -0600

    Update InsertionSort.rb

--- Program1/InsertionSort.rb
+++ Program1/InsertionSort.rb
@@ -1,5 +1,6 @@
-
-array = Array[1,7,4,8]
+# By: Mario Gonzalez
+# 12/7/2021
+# Desc: This program sorts arrays using the insertion sort method
 
 def insertion_sort(array)
     for i in 1...(array.length)
@@ -18,7 +19,9 @@ def insertion_sort(array)
     return array
 end
 
-array = Array[1,7,4,8]
+array = Array[10, 7, 4, 8, 80, 81, 45, 46, 58, 87, 6, 28, 72, 61, 31, 50, 76, 97, 78, 39,
+70, 40, 36, 14, 66, 1, 55, 38, 44, 29, 57, 37, 73, 5, 9, 34, 30, 53, 41, 86, 100, 26, 17, 95, 94, 82, 13, 4, 3, 83, 15, 67, 20, 33]
+
 
 p insertion_sort array
 
