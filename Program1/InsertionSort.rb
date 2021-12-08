0076f5cf9a484cea2dac93070fc06b645a609308
Lilgonzo <56660056+Lilgonzo@users.noreply.github.com>
2021-12-08 17:06:18 -0600

    Update InsertionSort.rb

--- Program1/InsertionSort.rb
+++ Program1/InsertionSort.rb
@@ -15,11 +15,10 @@ def insertionSort(arr)
     return arr
 end
 
-#unsorted array
+
 arr = Array[10, 7, 4, 8, 80, 81, 45, 46, 58, 87, 6, 28, 72, 61, 31, 50, 76, 97, 78, 39, 
 70, 40, 36, 14, 66, 1, 55, 38, 44, 29, 57, 37, 73, 5, 9, 34, 30, 53, 41, 86, 100, 26, 17, 95, 94, 82, 13, 4, 3, 83, 15, 67, 20, 33] 
 
-#prints the sorted array
 p insertionSort arr
 
 
