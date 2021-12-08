a259d7b4e3d7863d46c6ce983e5279217cbb2e38
Lilgonzo <56660056+Lilgonzo@users.noreply.github.com>
2021-12-08 14:55:14 -0600

    Update InsertionSort.rb

--- Program1/InsertionSort.rb
+++ Program1/InsertionSort.rb
@@ -2,28 +2,30 @@
 # 12/7/2021
 # Desc: This program sorts arrays using the insertion sort method
 
-def insertion_sort(array)
-    for i in 1...(array.length)
-        j = i 
-        while j > 0 
-            if array[j-1] > array[j]
-                temp = array[j]
-                array[j] = array[j-1]
-                array[j-1] = temp
+def insertionSort(arr)
+    for i in 1...(arr.length) #... includes the range of the array from 1
+        m = i 
+        while m > 0 
+            if arr[m-1] > arr[m]            #swaps the nums if the next one is smaller then the current one if not nothing happens since its sorted 
+                temp = arr[m]               #if not nothing happens since its sorted and it breaks
+                arr[m] = arr[m-1]
+                arr[m-1] = temp
             else
                 break
             end
-            j = j - 1
+            m = m - 1
         end
     end
-    return array
+    return arr
 end
 
-array = Array[10, 7, 4, 8, 80, 81, 45, 46, 58, 87, 6, 28, 72, 61, 31, 50, 76, 97, 78, 39,
-70, 40, 36, 14, 66, 1, 55, 38, 44, 29, 57, 37, 73, 5, 9, 34, 30, 53, 41, 86, 100, 26, 17, 95, 94, 82, 13, 4, 3, 83, 15, 67, 20, 33]
+#unsorted array
+arr = Array[10, 7, 4, 8, 80, 81, 45, 46, 58, 87, 6, 28, 72, 61, 31, 50, 76, 97, 78, 39, 
+70, 40, 36, 14, 66, 1, 55, 38, 44, 29, 57, 37, 73, 5, 9, 34, 30, 53, 41, 86, 100, 26, 17, 95, 94, 82, 13, 4, 3, 83, 15, 67, 20, 33] 
 
+#prints the sorted array
+p insertionSort arr
 
-p insertion_sort array
 
 
 
